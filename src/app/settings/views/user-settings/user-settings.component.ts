ccce66209f7762ad1b70d377914dad57589745af
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-11-02 21:13:25 +0000

    Allow users to change their password in-app

--- src/app/settings/views/user-settings/user-settings.component.ts
+++ src/app/settings/views/user-settings/user-settings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormContainer, FormFactory } from 'ngx-form-trooper';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from 'src/app/auth/services/auth/auth.service';
+import { HttpService } from 'src/app/core/services/http/http.service';
+import { IHttpError } from 'src/app/core/services/http/interfaces/http-error.interface';
 
 @Component({
   selector: 'app-user-settings',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings.component.scss']
 })
 export class UserSettingsComponent implements OnInit {
-
-  constructor() { }
+  public form: FormContainer;
+  public passwordChanged: boolean;
+  public errors: IHttpError;
+  constructor(
+    private readonly _formFactory: FormFactory,
+    private readonly _httpService: HttpService,
+    private readonly _authService: AuthService,
+    private readonly _spinnerService: NgxSpinnerService
+  ) {}
 
   ngOnInit(): void {
+    this.form = this._formFactory.createForm([
+      {
+        name: 'oldPassword',
+        label: 'Old password',
+        type: 'password',
+        validators: {
+          required: true
+        }
+      },
+      {
+        name: 'newPassword',
+        label: 'New Password',
+        type: 'password',
+        validators: {
+          required: true
+        }
+      }
+    ]);
   }
 
+  public async submitPasswordChange(): Promise<void> {
+    try {
+      this._spinnerService.show();
+      const { data } = await this._httpService.post(
+        'auth/changePassword',
+        this.form.value
+      );
+      this._authService.setJwt(data.jwt);
+      this.passwordChanged = true;
+    } catch ({ error }) {
+      this.errors = error;
+    } finally {
+      this._spinnerService.hide();
+    }
+  }
 }
