23753a9ac8e6dc175637466e8a66d5eb46faff7b
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-11-03 12:17:18 +0000

    Remove material dialog module from settings module

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,20 +3,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatDialogModule } from '@angular/material/dialog';
 import { NgxFormTrooperModule } from 'ngx-form-trooper';
 
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
+import {
+  DashboardSummaryConfigItemComponent,
+} from './components/dashboard-summary-config-item/dashboard-summary-config-item.component';
+import { FieldsConfigComponent } from './components/fields-config/fields-config.component';
+import {
+  MonthlyDistributionFieldsConfigComponent,
+} from './components/monthly-distribution-fields-config/monthly-distribution-fields-config.component';
 import { NetWorthConfigComponent } from './components/net-worth-config/net-worth-config.component';
 import { NetWorthSummaryConfigComponent } from './components/net-worth-summary-config/net-worth-summary-config.component';
 import {
   NetWorthSummaryFormModalComponent,
 } from './components/net-worth-summary-form-modal/net-worth-summary-form-modal.component';
 import { SettingsComponent } from './views/settings/settings.component';
-import { DashboardSummaryConfigItemComponent } from './components/dashboard-summary-config-item/dashboard-summary-config-item.component';
-import { MonthlyDistributionFieldsConfigComponent } from './components/monthly-distribution-fields-config/monthly-distribution-fields-config.component';
-import { FieldsConfigComponent } from './components/fields-config/fields-config.component';
 import { UserSettingsComponent } from './views/user-settings/user-settings.component';
 
 @NgModule({
@@ -38,8 +41,7 @@ import { UserSettingsComponent } from './views/user-settings/user-settings.compo
     FormsModule,
     ReactiveFormsModule,
     CoreModule,
-    NgxFormTrooperModule,
-    MatDialogModule
+    NgxFormTrooperModule
   ]
 })
 export class SettingsModule {}
