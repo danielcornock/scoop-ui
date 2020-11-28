1b61d21b318f7c905cb4cff93942157e4075cc76
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-11-28 02:32:41 +0000

    Hide floating action button on scroll

--- src/app/shared/components/floating-action-button/floating-action-button.component.ts
+++ src/app/shared/components/floating-action-button/floating-action-button.component.ts
@@ -1,11 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output
+} from '@angular/core';
+import { fromEvent, Subject, timer } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-floating-action-button',
   templateUrl: './floating-action-button.component.html',
   styleUrls: ['./floating-action-button.component.scss']
 })
-export class FloatingActionButtonComponent {
+export class FloatingActionButtonComponent implements OnInit, OnDestroy {
   @Input()
   public floatingActionButtonText: string;
 
@@ -15,7 +24,36 @@ export class FloatingActionButtonComponent {
   @Output()
   public floatingActionButtonAction: EventEmitter<void> = new EventEmitter();
 
+  public displayingAction = true;
+
+  private _destroy$: Subject<void> = new Subject();
+
+  ngOnInit(): void {
+    this._scrollSpy();
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
   public clickAction(): void {
     this.floatingActionButtonAction.emit();
+  }
+
+  private _scrollSpy(): void {
+    const app = document.querySelector('.App-content');
+
+    fromEvent(app, 'scroll')
+      .pipe(
+        takeUntil(this._destroy$),
+        switchMap(() => {
+          this.displayingAction = false;
+          return timer(1000);
+        })
+      )
+      .subscribe(() => {
+        this.displayingAction = true;
+      });
   }
 }
