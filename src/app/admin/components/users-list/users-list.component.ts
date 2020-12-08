c17e8a542ef91e9fd7430b898382872c941dd05c
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-12-08 20:31:23 +0000

    Improve admin area

--- src/app/admin/components/users-list/users-list.component.ts
+++ src/app/admin/components/users-list/users-list.component.ts
@@ -1,37 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { HttpService } from 'src/app/core/services/http/http.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
-export class UsersListComponent implements OnInit {
-  public users: Array<any>;
+export class UsersListComponent {
+  @Input()
+  public usersList: Array<any>;
 
-  constructor(
-    private readonly _httpService: HttpService,
-    private readonly _spinnerService: NgxSpinnerService
-  ) {}
+  @Output()
+  public usersListDelete = new EventEmitter<string>();
 
-  async ngOnInit(): Promise<void> {
-    try {
-      this._spinnerService.show();
-      const { data } = await this._httpService.get('admin/users');
-      this.users = data;
-    } finally {
-      this._spinnerService.hide();
-    }
-  }
-
-  public async deleteUser(id: string): Promise<void> {
-    try {
-      this._spinnerService.show();
-      await this._httpService.delete(`admin/users/${id}`);
-      this.users = this.users.filter((user) => user._id !== id);
-    } finally {
-      this._spinnerService.hide();
-    }
+  public deleteUser(id: string): void {
+    this.usersListDelete.emit(id);
   }
 }
--- src/app/admin/views/admin/admin.component.ts
+++ src/app/admin/views/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { HttpService } from 'src/app/core/services/http/http.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  public users: Array<any>;
 
-  constructor() { }
+  constructor(
+    private readonly _httpService: HttpService,
+    private readonly _spinnerService: NgxSpinnerService
+  ) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    try {
+      this._spinnerService.show();
+      const { data } = await this._httpService.get('admin/users');
+      this.users = data;
+    } finally {
+      this._spinnerService.hide();
+    }
   }
 
+  public async deleteUser(id: string): Promise<void> {
+    try {
+      this._spinnerService.show();
+      await this._httpService.delete(`admin/users/${id}`);
+      this.users = this.users.filter((user) => user._id !== id);
+    } finally {
+      this._spinnerService.hide();
+    }
+  }
 }
