2aeafd176815da72af0357ba4b4c1186a5fc6b99
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-12-10 23:08:45 +0000

    Make close type optional

--- src/app/lib/ngx-modal/components/modal-dialog-instance/modal-dialog-instance.component.ts
+++ src/app/lib/ngx-modal/components/modal-dialog-instance/modal-dialog-instance.component.ts
@@ -9,7 +9,7 @@ export abstract class ModalDialogInstanceComponent<
     private _data: TData
   ) {}
 
-  public close(data: TReturnData | undefined): void {
+  public close(data?: TReturnData): void {
     this._dialogRef.close(data);
   }
 
