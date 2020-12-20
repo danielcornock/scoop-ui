3e2c5bdf68ba958aa0b51be53e60f4270d9f2096
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-12-20 18:58:14 +0000

    Remove tests

--- src/app/core/services/http/http.service.spec.ts
+++ src/app/core/services/http/http.service.spec.ts
@@ -1,51 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { autoMock } from 'jasmine-automock';
-import { of } from 'rxjs';
 
-import { API_URL } from '../../providers/http.providers';
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
-  let service: HttpService,
-    dependencies: {
-      httpClient: jasmine.SpyObj<HttpClient>;
-    };
+  let service: HttpService;
 
   beforeEach(() => {
-    dependencies = {
-      httpClient: autoMock(HttpClient)
-    };
-    TestBed.configureTestingModule({
-      providers: [
-        { provide: HttpClient, useValue: dependencies.httpClient },
-        {
-          provide: API_URL,
-          useValue: 'baseurl/'
-        }
-      ]
-    });
+    TestBed.configureTestingModule({});
     service = TestBed.inject(HttpService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-
-  describe('get', () => {
-    let result: any;
-
-    beforeEach(() => {
-      dependencies.httpClient.get.and.returnValue(of('httpResponse'));
-      result = service.get('url');
-    });
-
-    it('should fetch the data', () => {
-      expect(dependencies.httpClient.get).toHaveBeenCalledWith('baseurl/url');
-    });
-
-    it('should return the value', async () => {
-      expect(await result).toBe('httpResponse');
-    });
   });
 });
